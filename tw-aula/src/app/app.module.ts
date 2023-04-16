4b2537f3f5465b433727df0f0293b9a8aa807646
SuayMack <pry_chu@hotmail.com>
2023-04-16 17:06:14 -0300

    create structure

--- tw-aula/src/app/app.module.ts
+++ tw-aula/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PostComponent } from './components/post/post.component';
+import { TwetComponent } from './components/twet/twet.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostComponent,
+    TwetComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ tw-aula/src/app/components/twet/twet.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-twet',
+  templateUrl: './twet.component.html',
+  styleUrls: ['./twet.component.css']
+})
+export class TwetComponent {
+
+}
